26e01f490831185c6620d9e5f1cc581d5dad1960
Alex Nazarkin <anazarkin1@gmail.com>
2015-06-06 12:54:51 -0400

    add loading cashflow.json;

--- Json_Data_Manager.py
+++ Json_Data_Manager.py
@@ -1,6 +1,22 @@
-__author__ = 'alex'
-
+import json
 
 class DataManager:
     def __init__(self):
-        pass
+        self._data_cashflow = ""
+
+        # list of types of cashflow available from json file
+        # ex: 'Operating Activities', 'Free',...
+        self._cashflow_types = []
+
+    def load_all_cashflow(self, filename="Data/cashflow.json"):
+        try:
+            with open(filename) as file:
+                self._data_cashflow = json.load(file)
+                return self.data_cashflow
+        except Exception as e:
+            print("Exception caught while loading cashflow.json with error:", e)
+            return None
+    
+    @property
+    def cashflow_types(self):
+        return self.cashflow_types
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from Stock_Analyzer import StockAnalyzer
 
+from Json_Data_Manager import DataManager
 def main():
     # yp = YahooParser()
     sa = StockAnalyzer()
+    dm = DataManager()
     # print( yp.scrape_total_cashflow("AAPL", "2015-01-01"))
     # print(yp.scrape_stock_price(["AAPL", "T"], "2010-11-11"))
     try:
-        print(sa.get_best_cashflow_performers_consec_periods(0.3, 3))
+        dm.load_all_cashflow()
+        ## print(sa.get_best_cashflow_performers_consec_periods(0.3, 3))
+
         # print( sa.get_best_return_performers_consec_periods(0.3,9) )
     except Exception as e:
         print(e)
