67a7a617d7cf7a27821201d3dc797518c90ba6cc
Alex Nazarkin <anazarkin1@gmail.com>
2015-06-07 17:11:18 -0400

    add save_cashflow_single to save single date/stock cashflow values in json format to Data/cashflow.json

--- Json_Data_Manager.py
+++ Json_Data_Manager.py
@@ -1,22 +1,65 @@
 import json
-
+from TimeHandler import *
 class DataManager:
     def __init__(self):
         self._data_cashflow = ""
 
         # list of types of cashflow available from json file
-        # ex: 'Operating Activities', 'Free',...
+        # ex: 'operating Activities', 'free',...
         self._cashflow_types = []
 
     def load_all_cashflow(self, filename="Data/cashflow.json"):
         try:
             with open(filename) as file:
                 self._data_cashflow = json.load(file)
-                return self.data_cashflow
+
+                if "cashflow" not in  self._data_cashflow.keys():
+                    self._data_cashflow["cashflow"]={}
+
+                self._cashflow_types=self._data_cashflow["cashflow"].keys()
+                return self._data_cashflow
         except Exception as e:
             print("Exception caught while loading cashflow.json with error:", e)
             return None
-    
+
+    def save_cashflow_single(self,cashflow_type, stock, date, value, overwrite=True, filename="Data/cashflow.json"):
+        '''
+
+        :param cashflow_type:
+        :param stock:
+        :param date: datetime object
+        :param value:
+        :param overwrite:
+        :return:
+        '''
+
+        cashflow_type=cashflow_type.lower()
+        value=float(value)
+        try:
+            date_str=get_date_to_string(date)
+        except Exception as e:
+            print("Exception caught while converting date to string with error:", e)
+
+        try:
+            if cashflow_type not in self._cashflow_types:
+                self._cashflow_types.append(cashflow_type)
+
+            if "cashflow" not in self._data_cashflow.keys():
+                self._data_cashflow["cashflow"]={}
+            if cashflow_type not in self._data_cashflow["cashflow"].keys():
+                self._data_cashflow["cashflow"][cashflow_type]={}
+
+            if  stock not in self._data_cashflow["cashflow"][cashflow_type].keys():
+                self._data_cashflow["cashflow"][cashflow_type][stock]={}
+
+            if (date_str not in self._data_cashflow["cashflow"][cashflow_type][stock].keys() or overwrite):
+                self._data_cashflow["cashflow"][cashflow_type][stock][date_str]=value
+                with open(filename, 'w') as file:
+                    file.write(json.dumps(self._data_cashflow,file, indent=4))
+
+        except Exception as e:
+            print ("Exception caught while saving single cashflow item for " +stock+" on date "+date_str+" with "
+                                                                                                       "error:", e)
     @property
     def cashflow_types(self):
         return self.cashflow_types
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Stock_Analyzer import StockAnalyzer
-
+from TimeHandler import *
 from Json_Data_Manager import DataManager
 def main():
     # yp = YahooParser()
@@ -8,7 +8,10 @@ def main():
     # print( yp.scrape_total_cashflow("AAPL", "2015-01-01"))
     # print(yp.scrape_stock_price(["AAPL", "T"], "2010-11-11"))
     try:
+        #testings:
         dm.load_all_cashflow()
+        dm.save_cashflow_single("Operating aCtiVities", "xyu", get_string_to_date_object("Oct 12 2011"),322.01)
+
         ## print(sa.get_best_cashflow_performers_consec_periods(0.3, 3))
 
         # print( sa.get_best_return_performers_consec_periods(0.3,9) )
