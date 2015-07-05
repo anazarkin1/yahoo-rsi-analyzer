85b342bc6a4b4a8fc86016054019032afa717061
Alex Nazarkin <anazarkin1@gmail.com>
2015-07-05 17:20:29 -0400

    NON-RUNNABLE : divide yahoo scraper into smaller subclasses accordingly;

--- Yahoo_Parser.py
+++ Yahoo_Parser.py
@@ -13,6 +13,30 @@ class YahooParser:
     def _get_json_obj(self,data):
         return json.loads(data)
 
+    def _convert_txt_to_float(self, txt):
+        """
+        Convert shortent text representation of a number to float
+        ie: 720M -> 720000000
+        :param txt: shortent text representation of a number that is needed to be converted
+        :return: a float number
+        """
+        txt = txt.upper()
+
+        formats = {"M":1000000, "B":1000000000}
+
+        try:
+            for chr in formats.keys():
+                pos = txt.find(chr)
+                if pos >-1:
+                    return float(txt[:pos]) * formats[chr]
+        except:
+            raise Exception("Error: can't convert {0} to a number representaiton, probably unknown format".format(txt))
+
+        raise Exception("Error: can't convert {0} to a number representaiton, probably unknown format".format(txt))
+
+
+
+
 
     def _scrape_yql(self, quote_list, yql_table ,required_date):
         """
@@ -56,46 +80,47 @@ class YahooParser:
         return output
 
 
-    def scrape(self, param, quote, type ,required_dates=None):
+    def scrape(self, param, quote, page,required_dates=None):
         """
         :param param: stock information to scrape
         :param quote: quote's symbol
-        :param type is a financial statement type or price  ("bs","is","cf","price")
+        :param page is a financial statement page or price  ("bs","is","cf","price")
         :param required_date:array of date objects(used primarily for prices);default is None to get all dates available
         :return:  array with:
                     [
                         {"date1":value1},
                         {"date2":value2}
                     ]
+        If param doesn't require date then return is:
+                {"" :value1}
         """
         param=param.lower()
-        type = type.lower()
+        page = page.lower()
         try:
-            if type == "cf":
+            if page == "cf":
                 url = "http://finance.yahoo.com/q/cf?s={0}".format(quote)
                 return self._scrape(url,param)
-            elif type == "bs":
+            elif page == "bs":
                 url = "http://finance.yahoo.com/q/bs?s={0}".format(quote)
                 return self._scrape(url,param)
-            elif type == "is":
+            elif page == "is":
                 url = "http://finance.yahoo.com/q/is?s={0}".format(quote)
                 return self._scrape(url,param)
-            elif type == "ks":
+            elif page == "ks":
                 url = "http://finance.yahoo.com/q/ks?s={0}".format(quote)
                 return self._scrape(url,param)
-
-            elif type == "price":
+            elif page == "price":
                 if len(required_dates) > 0:
                     return self._scrape_yql([quote],required_date=required_dates[0],yql_table="historicaldata")
                 else:
                     raise Exception("Error: date is required for price for quote: {0}".format(quote))
-            elif type == "dividends":
+            elif page == "dividends":
                 if len(required_dates) > 0:
                     return self._scrape_yql([quote],required_date=required_dates[0], yql_table="dividendhistory")
             else:
-                raise Exception("Exception: "+param+" is not supported yet")
+                raise Exception("Exception: "+param+" is not supported")
         except Exception as e:
-            print("Error while scraping for {0}".format(param))
+            print("Error while scraping for {0} with error: {1}".format(param, e))
 
 
     def _scrape(self, url, param):
@@ -110,27 +135,22 @@ class YahooParser:
         data=[]
         dates=[]
         try:
-            table = soup.find('table', attrs={'class': 'yfnc_tabledata1'})
-            if table is None:
-                raise Exception("Exception Caught: can't find table tr(Probably no {0} information)".format(param))
-            trs = table.table.find_all('tr')
-            # trs = [ele.text.strip().replace(",", "") for ele in trs_raw]
-            for tr in trs:
-                tds = tr.find_all('td')
-                pos = 1
-                for td in tds:
-                    if td.text.strip().lower() == param:
-                        data = [ele.text.strip().replace(",", "") for ele in tds[pos:]]
-                    else:
-                        pos += 1
+            #param_tag is the tag that has param as a text
+            #re.I paramater tells regex to search in case-insensitive mode
+            param_tag = soup.find(text = re.compile(param, re.I))
+            if param_tag is not None:
+                data = [sibling.text.strip().replace(",","") for sibling in param_tag.parent.parent.next_siblings]
+
+
 
             dates_td_tag = soup.find(text = re.compile("Period Ending"))
-            dates = [sibling.text.strip().replace(",","") for sibling in dates_td_tag.parent.parent.parent.next_siblings]
+            if dates_td_tag is not None:
+                dates = [sibling.text.strip().replace(",","") for sibling in dates_td_tag.parent.parent.parent.next_siblings]
 
         except Exception as e:
-            raise Exception("Exception Caught: can't find table tr(Probably no {0} information) with error: {1}".format(param, e))
+            raise Exception("Exception Caught: no {0} information or wrong param, with error: {2}".format(param, e))
 
-        #Regex used to remove '(',')' from cash flow numbers, since
+        #Regex used to remove '(',')' from numbers, since
         # negative values are placed inside '(', ')'
         #we want negatives to be in format '-value' to be used in calculations
         regex = re.compile('[(-,)]')
@@ -139,13 +159,18 @@ class YahooParser:
             if '(' in data[i]:
                 neg =True
             data[i]=regex.sub('', str(data[i]))
-            data[i] = float(data[i])
+            data[i] = self._convert_txt_to_float(data[i])
 
             if neg:
                 data[i] *= -1
 
 
-        return dict(zip(dates, data))
+        if len(dates)==0 and len(data) == 1:
+            return {"":data[0]}
+        elif len(dates)==0 and len(data) >1:
+            raise Exception("Error: there are two data points and no dates found for them while scraping for {0} ".format(param))
+        else:
+            return dict(zip(dates, data))
 
 
     def scrape_total_cashflow(self, quote):
@@ -160,4 +185,148 @@ class YahooParser:
         # DON'T USE THIS FUNCTION AS IT'S GOING TO GET REMOVED IN FUTURE VERSIONS
         #
         return self.scrape("Total Cash Flow From Operating Activities",quote,"cf")
+
+class ScrapeMain():
+    def get_json_obj(self,data):
+        return json.loads(data)
+
+    def get_request(self, url):
+
+        try:
+            r=requests.get(url)
+        except Exception as e:
+            print("Exception: scraping {0} with error: {1}".format(url, e))
+        return r
+
+    def get_soup(self, data):
+        return BeautifulSoup(data)
+
+    def convert_txt_to_float(self, txt):
+        """
+        Convert shortent text representation of a number to float
+        ie: 720M -> 720000000
+        :param txt: shortent text representation of a number that is needed to be converted
+        :return: a float number
+        """
+        txt = txt.upper()
+
+        formats = {"M":1000000, "B":1000000000}
+
+        try:
+            for chr in formats.keys():
+                pos = txt.find(chr)
+                if pos >-1:
+                    return float(txt[:pos]) * formats[chr]
+        except:
+            raise Exception("Error: can't convert {0} to a number representaiton, probably unknown format".format(txt))
+
+        raise Exception("Error: can't convert {0} to a number representaiton, probably unknown format".format(txt))
+
+    def changeNeg(self, data):
+        regex = re.compile('[(-,)]')
+        for i in range(0, len(data)):
+            neg = False
+            if '(' in data[i]:
+                neg =True
+            data[i]=regex.sub('', str(data[i]))
+            data[i] = self.convert_txt_to_float(data[i])
+
+            if neg:
+                data[i] *= -1
+        return data
+
+
+    def scrape(self, quote_list, page, required_date = None):
+        raise NotImplementedError("scrapemain is abstract, call its children classes")
+
+    def scrape(self, quote, param, page, required_date = None):
+        raise NotImplementedError("scrapemain is abstract, call its children classes")
+
+    def scrape(self, quote_list, required_date):
+        raise NotImplementedError("scrapemain is abstract, call its children classes")
+
+
+class ScrapeYql(ScrapeMain):
+    def __init__(self):
+        self.names = {"dividendhistory":"Dividends", "historicaldata":"Close"}
+
+    def prepare_url(self, quote_list, page, required_date):
+        yql_table = page
+        if len(quote_list)==1:
+            quote_list_str = str(tuple(quote_list))[:-2]+')'
+        else:
+            quote_list_str = str(tuple(quote_list))
+
+        try:
+            # get the friday's data if required date happens to be weekend
+            while required_date.isoweekday() in (6, 7):
+                # timedelta(1) equals to 1 day
+                required_date -= timedelta(1)
+            # Date format used for yql : "YYYY-MM-DD" also called isoformat
+            required_date_str = required_date.isoformat()
+        except Exception as e:
+            raise Exception("Exception caught while getting required_date string with error:, ", e)
+
+        return "http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance."+ yql_table +" where symbol in" + quote_list_str + " and startDate ='" + required_date_str + "' and endDate = '"+required_date_str\
+              + "'&env=store://datatables.org/alltableswithkeys&format=json"
+
+
+    def scrape(self, quote_list, required_date):
+        raise NotImplementedError("not implemented in scrapeyql, should use children classes")
+
+
+class ScrapeDividendHistory(ScrapeYql):
+    def scrape(self, quote_list, required_date):
+        tag_name = self.names["dividendhistory"]
+        url = self.prepare_url(quote_list, "dividendhistory",required_date)
+        r = self.get_request(url)
+        jo = self.get_json_obj(r.text)
+
+        output={}
+
+        try:
+            for i in jo["query"]["results"]['quote']:
+                output[i['Symbol']] = float(i[tag_name])
+        except Exception as e:
+            print("Exception Caught: ", e)
+        output["date"]= get_date_to_string(required_date)
+        return output
+
+
+class ScrapeYahoo(ScrapeMain):
+    def prepare_url(self, page, quote):
+        return "http://finance.yahoo.com/q/{0}?s={1}".format(page, quote)
+
+    def scrape(self, quote_list, param, page, required_date = None):
+        quote = quote_list[0]
+        url = self.prepare_url(page, quote)
+        r = self.get_request(url)
+
+
+
+class ScrapeYahooKS(ScrapeYahoo):
+    def __init__(self):
+        self.page = "ks"
+
+    def scrape(self, quote, param):
+        page = self.page
+        url = self.prepare_url(page, quote)
+        r = self.get_request(url)
+        soup = self.get_soup(r.text)
+
+        data = None
+
+        param_tag = soup.find(text = re.compile(param, re.I))
+        if param_tag is not None:
+            data = param_tag.parent.next_sibling.text.strip().replace(",","")
+        else:
+            raise Exception("Error: no data found for {0} param {1}".format(quote, param))
+
+        data = self.changeNeg(data)
+
+        return {quote: data}
+
+
+
+
 
