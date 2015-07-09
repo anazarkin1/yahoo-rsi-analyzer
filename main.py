a5736fdbe0a886176e6dc0579f9f9b9f5067de90
anazarkin1 <anazarkin1@gmail.com>
2015-07-09 13:35:54 -0400

    add cashflow scrape support for all params(tested on AAPL); fix a bug with negative numbers are not being handled in a proper way;

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from Stock_Analyzer import StockAnalyzer
 from TimeHandler import *
 from Json_Data_Manager import DataManager
 from Console import Console
+from Yahoo_Parser import *
 def main():
-    # yp = YahooParser()
+    yp = ScrapeYahooCF()
     sa = StockAnalyzer()
     dm = DataManager()
     console=Console()
@@ -11,54 +12,57 @@ def main():
     # print(yp.scrape_stock_price(["AAPL", "T"], "2010-11-11"))
     try:
         #testings:
-        # dm.save_cashflow_single("Operating aCtiVities", "xyu", get_string_to_date_object("Oct 12 2011"),322.01)
-        while True:
-            console.print("To exit type 'exit'")
-            console.print("Type number from below:")
-            console.print("1) Get best cashflow performers (best percentage, number of periods, force update)")
-            console.print("2) Get best price performers (best percentage, number of periods, force update)")
-            console.print("3) Get best price and cashflow performers (best percentage, number of periods, "
-                          "force update)")
-            choice = console.ask_value(">>")
-            if choice=='exit':
-                return
-            elif choice=='1':
-                best_percentage=console.ask_value("best percentage:")
-                numb_periods=console.ask_value("number of periods:")
-                force_update_tmp=console.ask_value("force update (y/n):")
-
-                if force_update_tmp=='y':
-                    force_update=True
-                else:
-                    force_update=False
-
-                console.print("Calculating...")
-                console.pprint(sa.get_best_cashflow_performers_consec_periods(best_percentage,
-                                                                                         numb_periods,force_update) )
-            elif choice=='2':
-                best_percentage=console.ask_value("best percentage:")
-                numb_periods=console.ask_value("number of periods:")
-                force_update_tmp=console.ask_value("force update (y/n):")
-
-                if force_update_tmp=='y':
-                    force_update=True
-                else:
-                    force_update=False
-
-                console.print("Calculating...")
-                console.print("Result:\n")
-                console.pprint(sa.get_best_prices_performers_consec_periods(best_percentage,
-                                                                                         numb_periods,force_update) )
-
-            elif choice=='3':
-                console.print("Not yet implemented")
-            else:
-                console.print("Wrong argument, try again")
+        print( yp.scrape("AAPL","Depreciation") )
 
 
-        # print(sa.get_best_cashflow_performers_consec_periods(0.3, 3, overwrite_file=True))
 
-        print( sa.get_best_prices_performers_consec_periods(0.3,9, overwrite=True) )
+        # while True:
+        #     console.print("To exit type 'exit'")
+        #     console.print("Type number from below:")
+        #     console.print("1) Get best cashflow performers (best percentage, number of periods, force update)")
+        #     console.print("2) Get best price performers (best percentage, number of periods, force update)")
+        #     console.print("3) Get best price and cashflow performers (best percentage, number of periods, "
+        #                   "force update)")
+        #     choice = console.ask_value(">>")
+        #     if choice=='exit':
+        #         return
+        #     elif choice=='1':
+        #         best_percentage=console.ask_value("best percentage:")
+        #         numb_periods=console.ask_value("number of periods:")
+        #         force_update_tmp=console.ask_value("force update (y/n):")
+        #
+        #         if force_update_tmp=='y':
+        #             force_update=True
+        #         else:
+        #             force_update=False
+        #
+        #         console.print("Calculating...")
+        #         console.pprint(sa.get_best_cashflow_performers_consec_periods(best_percentage,
+        #                                                                                  numb_periods,force_update) )
+        #     elif choice=='2':
+        #         best_percentage=console.ask_value("best percentage:")
+        #         numb_periods=console.ask_value("number of periods:")
+        #         force_update_tmp=console.ask_value("force update (y/n):")
+        #
+        #         if force_update_tmp=='y':
+        #             force_update=True
+        #         else:
+        #             force_update=False
+        #
+        #         console.print("Calculating...")
+        #         console.print("Result:\n")
+        #         console.pprint(sa.get_best_prices_performers_consec_periods(best_percentage,
+        #                                                                                  numb_periods,force_update) )
+        #
+        #     elif choice=='3':
+        #         console.print("Not yet implemented")
+        #     else:
+        #         console.print("Wrong argument, try again")
+        #
+        #
+        # # print(sa.get_best_cashflow_performers_consec_periods(0.3, 3, overwrite_file=True))
+        #
+        # print( sa.get_best_prices_performers_consec_periods(0.3,9, overwrite=True) )
     except Exception as e:
         print(e)
 
