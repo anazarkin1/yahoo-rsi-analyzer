32682f1d4d42538ab6a2ca04a2aa1dd0d4c9f9ce
Alex Nazarkin <anazarkin1@gmail.com>
2015-08-04 12:34:41 -0400

    Fix problem with saving empty stock information; Thres still a potential problem with opening json data file that has no data in it;

--- JsonDataManager.py
+++ JsonDataManager.py
@@ -24,10 +24,14 @@ class DataManager:
         try:
             f = open(self.filename, 'r+')
             self.data = json.load(f).copy()
+            f.close()
 
         except FileNotFoundError:
             #file doesn't exist, create it
             f = open(self.filename, 'w')
+            f.close()
+        except Exception as e:
+            print("Error: loading json data file, with error: {0}".format(e))
 
         #check if file's content is valid json
         if type(self.data) != dict:
@@ -82,6 +86,9 @@ class DataManager:
         :param str_data:
         :return:
         """
+        if len(str_data) ==0:
+            return
+
         if type(self.data) is not dict:
             self.data={}
 
